4d9607b67aadb535ea452b7a307e2c6cdd8fb152
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-06-07 20:32:59 -0700

    site build

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,104 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <span class="body">
+    <img class="niels" alt="niels" src="/niels_bw.jpg" />
+    <img class="map" alt="map" src="/map.svg" />
+    <br />
+    <br />
+    <div class="grey">
+      <h1>Dan Humphries</h1>
+      <p>Freelance web engineer • multidisplinary Artist</p>
+      <p>ldn • pdx</p>
+      <p>-</p>
+    </div>
+    <div class="grey body__left">
+      <h3>web</h3>
+      <p>hedron hackerspace • web • 2022</p>
+    </div>
+    <div class="grey body__right">
+      <h3>art</h3>
+      <p>metamodernism as a service • instagram • 2021</p>
+      <p>vivi (mas + theremin) • bauhaus fm • 2021</p>
+      <p>computational soul • bartlett school of architecture • 2022</p>
+    </div>
+    <div class="grey body__left">
+      <h3>wip</h3>
+      <!--p>die natur • pdx</p-->
+      <p>conversations • essay</p>
+      <p>pandora's box • twitter</p>
+      <p>signalling dissent • photo essay</p>
+      <p>rubik's world • kings college london</p>
+    </div>
+    <div class="body__right">
+      <h3>contact</h3>
+      <p>
+        <a href="https://www.instagram.com/dn.niels">instagram</a>
+        •
+        <a href="mailto: dan@aufbau.io">email</a>
+        •
+        <a href="https://www.linkedin.com/in/dan-humphries/">linkedin</a>
+      </p>
+    </div>
+    <br />
+    <br />
+  </span>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    margin-top: 3rem;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    font-family: "dahlia", sans-serif;
+    color: #171717;
+    font-size: 3em;
+    font-weight: 100;
+    margin: 1rem 0 2.5rem 0;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h3 {
+    font-family: "dahlia", sans-serif;
+    color: #171717;
+  }
+
+  .map {
+    position: absolute;
+    width: 100%;
+    width: 100%;
+    left: 0;
+    top: 10rem;
+    transform: translateY(-60%);
+    z-index: -1;
+  }
+
+  .body {
+    display: float;
+  }
+
+  .grey {
+    background: #f0f0f0;
+  }
+
+  .body__left {
+    text-align: left;
+  }
+
+  .body__right {
+    margin: 0.1em;
+    text-align: right;
+  }
+
+  @media (max-width: 800px) {
+    .niels {
+      width: 100%;
+    }
+
+    .map {
+      width: 200%;
+    }
+  }
 </style>
